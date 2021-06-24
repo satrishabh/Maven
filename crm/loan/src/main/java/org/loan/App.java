c95cf262b6a5e17ccd8b2a5d729f0a2c50112574
Administrator <Administrator@host.docker.internal>
2021-06-25 03:01:40 +0530

    Second Commit

---
+++ crm/loan/src/main/java/org/loan/App.java
@@ -0,0 +1,13 @@
+package org.loan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
